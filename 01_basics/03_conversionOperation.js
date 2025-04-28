f58ff27c8394d0001569d6942d78ae8ff95be955
sujal1130 <sujalsarodiya758@gmail.com>
2025-04-28 12:56:05 +0530

    learn javascript conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// I learn "chai aur code" youtube
+
+// let score = "11";
+// let score = "11abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+let score = "Sujal";
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// OUTPUT //
+//"11"       => 11
+//"11abc"    => NaN
+// true      => 1; false => 0;
+// null      => 0
+// undefined => NaN
+// "Sujal"   => NaN
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "";
+let isLoggedIn = "sujal";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// OUTPUT //
+//1       => true;
+//0       => false;
+//""      => false;
+//"sujal" => true
+
+let someNumber = 11;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
