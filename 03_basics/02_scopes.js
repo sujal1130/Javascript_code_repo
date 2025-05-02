3034a0854c8d4fa8e2d6af0c346323adc4554557
sujal1130 <sujalsarodiya758@gmail.com>
2025-05-02 11:51:53 +0530

    scopes updata

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,3 +11,41 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+  const username = "sujal";
+
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  //   console.log(website);
+
+  two();
+}
+// one();
+
+if (true) {
+  const username = "sujal";
+  if (username === "sujal") {
+    const website = " youtub";
+    // console.log(username + website);
+  }
+  //   console.log(website);
+}
+// console.log(username);
+
+// ++++++++++++++++++++ interesting ++++++++++++++++++++++++++ //
+
+// console.log(addone(5));
+function addone(num) {
+  return num + 1;
+}
+// addone(5);
+
+// addTwo(5);
+const addTwo = function (num) {
+  return num + 2;
+};
+addTwo(5);
+// console.log(addTwo(5));
