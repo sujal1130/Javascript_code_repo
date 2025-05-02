b3c6ad79c22c14a287e90860af2234f8de8d79eb
sujal1130 <sujalsarodiya758@gmail.com>
2025-05-02 11:15:24 +0530

    updata functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -64,4 +64,33 @@ function loginUserMessage(usersname) {
 }
 
 // console.log(loginUserMessage("sujal"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+// console.log(calculateCartPrice(200, 300, 400, 2000));
+
+const user = {
+  usersname: "sujal",
+  price: 199,
+};
+
+function hanleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.usersname} and price ${anyobject.price}`
+  );
+}
+// hanleObject(user);
+hanleObject({
+  usersname: "sam",
+  price: 399,
+});
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 600]));
