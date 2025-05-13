35df734dc6f29bd87f0ed0bd00f1b77ac9c43b2b
sujal1130 <sujalsarodiya758@gmail.com>
2025-05-13 09:39:10 +0530

    forin  add

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+  js: "javascript",
+  cpp: "c++",
+  rb: "ruby",
+  swift: "swift by apple",
+};
+
+for (const key in myObject) {
+  //   console.log(key);
+  //   console.log(myObject[key]);
+  //   console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"];
+
+for (const key in programming) {
+  //   console.log(key);
+  //   console.log(programming[key]);
+}
+
+const map = new Map();
+map.set("IN", "India");
+map.set("USA", "United States of America");
+map.set("FR", "France");
+map.set("IN", "India");
+
+for (const key in map) {
+  //   console.log(key);
+}
